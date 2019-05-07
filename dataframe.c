d144dd8b2b854098ca06096355022e96ccc3524b
Magyar Máté <hunyeti@gmail.com>
2019-05-08 00:52:45 +0200

    add inital implementation of data framing

---
+++ dataframe.c
@@ -0,0 +1,212 @@
+#include "dataframe.h"
+#include "error_log.h"
+#include "safe_array.h"
+
+#define SEND_ESCAPE(data, needs_escape) \
+           switch (data) { \
+            case FRAME_START: \
+            case FRAME_ESC: \
+            case FRAME_END: \
+            case FRAME_END_CRC32: \
+                needs_escape \
+            break; \
+            default: \
+                break; \
+        } \
+
+Frame_Receive_buffer receive_buffer;
+
+void (*crc_feed)(u8 data) = NULL;
+u32 (*crc_get)() = NULL;
+
+void reset_buffer(Frame_Receive_buffer * receive_buffer, bool start) {
+    receive_buffer->buffer_cursor = 0;
+    receive_buffer->received_end = false;
+    receive_buffer->received_start = start;
+    receive_buffer->received_esc = false;
+    receive_buffer->crc_counter = 0;
+    receive_buffer->crc = 0;
+
+}
+bool receive_byte(u8 data, Frame_Receive_buffer * receive_buffer) {
+    if (receive_buffer->received_start && !receive_buffer->received_end) {
+
+        if (receive_buffer->crc_counter) {
+            receive_buffer->crc_counter--;
+            receive_buffer->crc = receive_buffer->crc << 8;
+            receive_buffer->crc |= data;
+            if (!receive_buffer->crc_counter) {
+                receive_buffer->received_end = true;
+                loop(i, receive_buffer->buffer_cursor)
+                {
+                    crc_feed(receive_buffer->buffer[i]);
+                }
+                u32 crc_result = crc_get();
+                if (crc_result != receive_buffer->crc) {
+                    log_error(EC_FRAME_RECEIVE_CRC_MISMATCH, 'r');
+
+                }
+                reset_buffer(receive_buffer, false);
+                return true;
+            }
+        }
+        else if (!receive_buffer->received_esc) {
+            if (data == FRAME_START) {
+                log_error(EC_FRAME_RECEIVED_START_WHILE_MESSAGE_STILL_IN_PROGRESS, 'd');
+                //On error, just ignore the current frame and start again
+                reset_buffer(receive_buffer, true);
+            } else if (data == FRAME_END_CRC32) {
+                receive_buffer->crc_counter = 4;
+                //Receive CRC, and than transmission is finished
+            } else if (data == FRAME_END) {
+                //End transmission
+                receive_buffer->received_end = true;
+                return true;
+            } else if (data == FRAME_ESC) {
+                receive_buffer->received_esc = true;
+            } else {
+                //Regular byte
+                receive_buffer->buffer[receive_buffer->buffer_cursor] = data;
+                receive_buffer->buffer_cursor++;
+            }
+        } else {
+            receive_buffer->buffer[receive_buffer->buffer_cursor] = data;
+            receive_buffer->buffer_cursor++;
+            //Once we received the escaped byte we reset the escaping
+            receive_buffer->received_esc = false;
+        }
+    } else if (data == FRAME_START) {
+        //TODO maybe check if previous finished
+        reset_buffer(receive_buffer, true);
+    } else {
+        log_error(EC_FRAME_RECEIVED_UNEXPECTED_BYTE, (char) data);
+
+    }
+
+    return false;
+}
+
+u16 send_on_the_fly_no_crc(u8 * data, u16 size, void (*send_byte)(u8 byte)) {
+    send_byte(FRAME_START);
+
+    loop(i, size)
+    {
+        u8 cb = data[i];
+        SEND_ESCAPE(cb, send_byte(FRAME_ESC););
+        send_byte(cb);
+    }
+    send_byte(FRAME_END);
+
+    return size;
+}
+u16 send_on_the_fly(u8 * data, u16 size, void (*send_byte)(u8 byte)) {
+
+    /** Check is CRC functions are set **/
+    if (crc_feed == NULL || crc_get == NULL) {
+        log_error(EC_FRAME_SEND_CRC_FUNCTIONS_NOT_SET, 'f');
+        send_on_the_fly_no_crc(data, size, send_byte);
+    }
+
+    /** Send start of the frame byte **/
+    send_byte(FRAME_START);
+
+    loop(i, size)
+    {
+        u8 cb = data[i];
+        //If it's an escaped character, send an escape byte before sending the data;
+        SEND_ESCAPE(cb, send_byte(FRAME_ESC););
+        /** Send byte through the arbitrary send function **/
+        send_byte(cb);
+        /** Feed each byte to the crc calculator function **/
+        crc_feed(cb);
+
+    }
+    /** Send end of frame with a 32 bit crc **/
+    send_byte(FRAME_END_CRC32);
+    /** Get result from crc calculator**/
+    u32 crc = crc_get();
+    /** Send crc, MSB first **/
+    send_byte(crc >> 24 & 0xFF);
+    send_byte((crc >> 16) & 0xFF);
+    send_byte((crc >> 8) & 0xFF);
+    send_byte(crc & 0xFF);
+
+    return size;
+}
+
+u16 frame_bytes(u8 * in, u16 in_length, u8 * out, u16 buffer_size, bool add_crc) {
+    u16 out_count = 0;
+    /** The maximum the buffer should be filled while sending that,
+     * this includes the 2 bytes for escaping, and if needed 5 bytes for end and crc
+     * or 1 byte for end frame **/
+    const u16 out_limit = buffer_size - 2 - (add_crc ? 5 : 1);
+    out[out_count++] = FRAME_START;
+
+    loop(i, in_length)
+    {
+        if (out_count >= out_limit) {
+            log_error(EC_FRAME_SEND_BUFFER_OVERFLOW, 'f');
+            return 0;
+        }
+
+        u8 cb = in[i];
+        SEND_ESCAPE(cb, out[out_count++] = FRAME_ESC;);
+        out[out_count++] = in[i];
+
+        /** TODO, this maybe should not be in an if **/
+        if (add_crc && crc_feed && crc_get) {
+            crc_feed(cb);
+        }
+    }
+    if (add_crc) {
+        out[out_count++] = FRAME_END_CRC32;
+        /** Get result from crc calculator**/
+        u32 crc = crc_get();
+        /** Send crc, MSB first **/
+        out[out_count++] = (crc >> 24 & 0xFF);
+        out[out_count++] = ((crc >> 16) & 0xFF);
+        out[out_count++] = ((crc >> 8) & 0xFF);
+        out[out_count++] = (crc & 0xFF);
+    } else {
+        out[out_count++] = FRAME_END;
+    }
+
+    return out_count;
+}
+
+/**
+ * TODO do something to run these automatically.
+ */
+void unit_test() {
+    Frame_Receive_buffer bfr;
+    reset_buffer(&bfr, false);
+    u8 * abc = (u8 *) "HEY SEXY";
+    u8 zzz[64] = { };
+
+    u16 wrote_bytes = frame_bytes(abc, 8, zzz, 64, true);
+    if (wrote_bytes != 14) {
+        // failed
+    }
+
+    loop(i, wrote_bytes)
+    {
+        //This should run without a problem
+        receive_byte(zzz[i], &bfr);
+    }
+
+    loop(i,8)
+    {
+        if (abc[i] != bfr.buffer[i]) {
+            //Test failed
+        }
+    }
+
+    zzz[4] = 'z';
+
+    loop(i, wrote_bytes)
+    {
+        //This should generate a crc mismatch
+        receive_byte(zzz[i], &bfr);
+    }
+
+}
---
+++ dataframe.h
@@ -0,0 +1,58 @@
+#ifndef UTIL_DATAFRAME_H_
+#define UTIL_DATAFRAME_H_
+#include "types.h"
+#include "stdbool.h"
+
+#define FRAME_START 0xFA
+#define FRAME_END_CRC32 0xFB
+#define FRAME_END 0xFC
+#define FRAME_ESC 0XFD
+
+#ifndef FRAME_BUFFER_SIZE
+#define FRAME_BUFFER_SIZE 128
+#endif
+
+typedef struct {
+    bool received_start;
+    bool received_end;
+    bool received_esc;
+    u8 buffer[FRAME_BUFFER_SIZE];
+    u32 crc;
+    u32 crc_counter;
+    u16 buffer_cursor;
+} Frame_Receive_buffer;
+
+/**
+ * Returns true if transmission is finished
+ */
+bool receive_byte(u8 data, Frame_Receive_buffer * receive_buffer);
+
+/**
+ * Returns the number of bytes in `out`
+ * if it's 0, it means an error
+ */
+u16 frame_bytes(u8 * in, u16 in_length, u8 * out, u16 buffer_size, bool add_crc);
+
+/**
+ * Calls send_byte for every encoded byte, uses no intermediate buffer
+ *
+ */
+u16 send_on_the_fly(u8 * data, u16 size, void (*send_byte)(u8 byte));
+
+/**
+ * Same a send_on_the_fly, but it doesn't send the crc, faster but less relaible
+ */
+u16 send_on_the_fly_no_crc(u8 * data, u16 size, void (*send_byte)(u8 byte));
+
+/**
+ * This will be used to feed data into the crc calculator
+ * This needs to be pointed to a function if sending crc is used
+ */
+void (*crc_feed)(u8 data);
+
+/**
+ * This variable needs to point to a crc function that returns standard CRC32
+ * The string of 8 bytes "HEY SEXY" should calculate 0xAC15FBBF
+ */
+u32 (*crc_get)();
+#endif /* UTIL_DATAFRAME_H_ */
